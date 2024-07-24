6bc64e793ac97e9b4114eeb0820bff902aa0ee69
Mon <tracer.coolkid@gmail.com>
2024-07-24 14:38:17 +0700

    feat: add ex1-2

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,35 @@
 function findStudentIndex(students, searchStudent) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
-}
-
-// ตอบคำถามตรงนี้จ้า
+	let indexStudent = -1;
+	let left = 0;
+	let right = students.length - 1;
+  
+	while (left <= right) {
+	  const mid = Math.floor((left + right) / 2);
+	  if (students[mid] === searchStudent) {
+		return mid;
+	  } else if (students[mid] < searchStudent) {
+		left = mid + 1;
+	  } else {
+		right = mid - 1;
+	  }
+	}
+  
+	return indexStudent;
+  }
+  
+  students = [
+	"Andrew",
+	"Bobby",
+	"Diana",
+	"George",
+	"Hannah",
+	"Isaac",
+	"Jasmine",
+	"John",
+  ];
+  students2 = ["Alice", "Bob", "Charlie", "Diana", "Ethan", "Fiona"];
+  
+  console.log(findStudentIndex(students, "John"));
+  console.log(findStudentIndex(students2, "Andrew"));
+  // ตอบคำถามตรงนี้จ้า
+// O(log n) เพราะ จำนวนรอบการทำงานจะเพิ่มขึ้นตามขนาดข้อมูล Input
--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,35 @@
 function findBookIndex(books, searchTitle) {
   // เริ่มเขียนโค้ดตรงนี้จ้า
+  let indexBook = -1;
+  let left = 0;
+  let right = books.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (books[mid].title === searchTitle) {
+      return mid;
+    } else if (books[mid].title < searchTitle) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return indexBook;
 }
+
+books = [
+  { title: "A Tale of Two Cities", author: "Charles Dickens" },
+  { title: "Brave New World", author: "Aldous Huxley" },
+  { title: "Catch-22", author: "Joseph Heller" },
+  { title: "Don Quixote", author: "Miguel de Cervantes" },
+  { title: "Ender's Game", author: "Orson Scott Card" },
+  { title: "Fahrenheit 451", author: "Ray Bradbury" },
+  { title: "Gone with the Wind", author: "Margaret Mitchell" },
+  { title: "Harry Potter", author: "J.K. Rowling" },
+];
+
+searchBook1 = "Gone with the Wind";
+searchBook2 = "The Master Mind";
+
+console.log(findBookIndex(books, searchBook1));
+console.log(findBookIndex(books, searchBook2));
